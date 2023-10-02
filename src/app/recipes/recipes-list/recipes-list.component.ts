5cd647047b780edff7ee5f1dcd2d67d26acc2ca3
Matt Tucker <arcanemahuloq@gmail.com>
2023-10-01 19:13:09 -0500

    Added content to recipe component and recipe details

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
   styleUrls: ['./recipes-list.component.css'],
 })
 export class RecipesListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This Is Simply A Test',
+      'https://thecozycook.com/wp-content/uploads/2023/02/Homemade-Ramen-1-.jpg'
+    ),
+  ];
 
   constructor() {}
 
