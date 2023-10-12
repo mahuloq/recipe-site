fee8f0cf5d4e2ca0cb8e8083dd7c72e78d9645b2
Matt Tucker <arcanemahuloq@gmail.com>
2023-10-12 15:18:25 -0500

    Can push ingredients from recipe to shopping lisT

--- src/app/recipes/recipes-detail/recipes-detail.component.ts
+++ src/app/recipes/recipes-detail/recipes-detail.component.ts
@@ -1,11 +1,20 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipes-detail',
   templateUrl: './recipes-detail.component.html',
   styleUrls: ['./recipes-detail.component.css'],
 })
-export class RecipesDetailComponent {
+export class RecipesDetailComponent implements OnInit {
   @Input() recipe: Recipe;
+
+  constructor(private recipeService: RecipeService) {}
+
+  ngOnInit(): void {}
+
+  onAddToShoppingList() {
+    this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
 }
