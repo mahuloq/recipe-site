b55ae5f93f001f36de4f9cfa35bb01b87862c94b
Matt Tucker <arcanemahuloq@gmail.com>
2023-11-03 16:12:28 -0500

    Added signup feature

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
 })
 export class AuthComponent {
+  constructor(private authService: AuthService) {}
+
   isLoginMode = true;
 
   onSwitchMode() {
@@ -13,7 +16,25 @@ export class AuthComponent {
   }
 
   onSubmit(form: NgForm) {
-    console.log(form.value);
+    if (!form.valid) {
+      return;
+    }
+
+    if (this.isLoginMode) {
+      // ...
+    } else {
+      const email = form.value.email;
+      const password = form.value.password;
+
+      this.authService.signUp(email, password).subscribe(
+        (resData) => {
+          console.log(resData);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    }
     form.reset();
   }
 }
